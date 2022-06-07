c4ee490882ceb675de9933feeae29e5cde6c0c95
YanaSkorb <skorbyaras29@gmail.com>
2022-06-07 13:24:58 +0300

    Initial commit

---
+++ zad3/zad3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace  zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Первое имя: ");
+            string a = Console.ReadLine();
+            Console.Write("Второе имя: ");
+            string b = Console.ReadLine();
+            info Men = new info(a);
+            Console.Write("Неизменное имя: ");
+            Console.WriteLine(Men.imya);
+            Men.inform += vivod;
+            Men.Put(b);
+            Console.WriteLine($"Измененное имя: {Men.imya}");
+            Men.inform -= vivod;
+            Console.Read();
+        }
+        private static void vivod(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+class info
+{
+    public delegate void muinfo(string message);
+    public event muinfo inform;
+    public info(string imya)
+    {
+        this.imya = imya;
+    }
+    public string imya { get; private set; }
+    public void Put(string ima)
+    {
+        imya = ima;
+        inform?.Invoke($"Поменять имя на: {ima}");
+    }
+}
